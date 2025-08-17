76fe37e16587b85cefac3505df45d605a223408b
Max Amelchenko <maxamel2002@gmail.com>
2025-08-17 16:54:06 +0300

    Update s3.tf

--- terraform/aws/s3.tf
+++ terraform/aws/s3.tf
@@ -141,3 +141,14 @@ resource "aws_s3_bucket" "logs" {
     yor_trace            = "01946fe9-aae2-4c99-a975-e9b0d3a4696c"
   })
 }
+
+resource "aws_s3_bucket" "my_good_example" {
+  for_each = var.bucket_names
+
+  bucket = each.key
+
+  tags = {
+    Name        = each.key
+    Environment = "Dev"
+  }
+}
